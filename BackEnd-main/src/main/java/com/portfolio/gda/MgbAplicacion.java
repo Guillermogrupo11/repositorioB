94c794f35a91f4dda4abde8a680c059042693b43
Guillermo <guillermo.arena@gmail.com>
2022-07-29 17:21:03 -0300

    pasando a 2 repo B Back

---
+++ BackEnd-main/src/main/java/com/portfolio/gda/MgbAplicacion.java
@@ -0,0 +1,20 @@
+
+package com.portfolio.gda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author guill
+ */
+@SpringBootApplication
+
+public class MgbAplicacion {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgbAplicacion.class, args);
+	}
+
+}
